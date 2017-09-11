de87df727726cb1010caf6717818719a29722c37
NickR23 <nickrich2624@gmail.com>
2017-09-10 20:42:01 -0400

    Initial commit

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -7,9 +7,100 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Bnode{
+private:
+    int data;
+    Bnode *left;
+    Bnode *right;
+public:
+    Bnode(char d, Bnode *l, Bnode *r){data=d,left=l,right=r;}
+    char getData(){return(data);}
+    Bnode *getLeft(){return(left);}
+    Bnode *getRight(){return(right);}
+    void setLeft(Bnode *l){left=l;}
+    void setRight(Bnode *r){right=r;}
+};
+
+class List{
+private:
+    Bnode *first;
+public:
+    List(){first=NULL;}
+    void print(Bnode *f);
+    void insert(string);
+};
+
+void List::insert(string input){
+    Bnode *current;
+    char data;
+    int size=input.size();
+    cout << "String: "<<input<<endl;
+    for(int i=0;i<size;i++){
+        data=input[i];
+    
+        Bnode *newbie=new Bnode(data,NULL,NULL);
+        
+        //if there is nothing in TREE
+        if(first==NULL){
+            first=newbie;
+            current=first;
+
+        }
+        //if there is anything in TREE
+        else{
+            current=first;
+            
+            do{
+                if( (newbie->getData() <= current->getData()) && (current->getLeft() != NULL )){
+                    current=current->getLeft();
+                }
+                else if ((newbie->getData() > current->getData()) && (current->getRight() != NULL)){
+                    current=current->getRight();
+                    
+                }
+            
+            
+            }while( (current->getLeft()!=NULL && newbie->getData() <= current->getData()) || (current->getRight()!=NULL && newbie->getData() > current->getData()));
+            
+            
+            if(newbie->getData()>current->getData()){
+                current->setRight(newbie);
+                cout<<"## "<<current->getData()<<" "<<current<<" || "<<newbie->getData()<<" "<<newbie<<endl;
+            }
+            else{
+                current->setLeft(newbie);
+                cout<<"$$"<<current->getData()<<" "<<current<<" || "<<newbie->getData()<<" "<<newbie<<endl;
+            }
+        }
+
+        
+        
+    }
+    cout<<"Sorted string: ";
+    print(first);
+    cout<<endl;
+}
+
+void List::print(Bnode *first){
+    cout<<first->getData()<<",";
+    if(first->getLeft() != NULL){
+        print(first->getLeft());
+    }
+    if(first->getRight() != NULL){
+        print(first->getRight());
+    }
+}
+
+
+
+
+int main() {
+    List l;
+    string input;
+    cout << "Input string: \n";
+    cin >> input;
+    l.insert(input);
     return 0;
 }
